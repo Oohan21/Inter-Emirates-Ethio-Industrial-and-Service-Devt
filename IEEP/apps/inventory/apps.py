91b25efccaea81fb0699537a2295ff09413647b5
heaven477 <esromlema477@gmail.com>
2025-11-10 10:48:17 +0300

    Update apps.py

--- IEEP/apps/inventory/apps.py
+++ IEEP/apps/inventory/apps.py
@@ -5,3 +5,5 @@ class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.inventory'
 
+    def ready(self):
+        import apps.inventory.signals
